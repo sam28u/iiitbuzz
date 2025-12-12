1680e93d367b3314431c8cff5f8a6b18e3d0532e
Spandan Hota <spandanhota2005@outlook.com>
2025-12-12 21:38:53 +0530

    fix: css variables
    Signed-off-by: Spandan Hota <spandanhota2005@outlook.com>

--- apps/web/src/routes/_index.tsx
+++ apps/web/src/routes/_index.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/ui/header";
 
 export default function HomePage() {
 	return (
-		<div className="flex flex-col min-h-screen">
+		<div className="flex flex-col min-h-screen landing-theme">
 			<Header />
 			<div className="relative flex-1">
 				<main className="relative z-10">
